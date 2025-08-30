15cf31eccf66809fa1e4f626d575bc1aac6582bc
Giliarde da Silva Rodrigues <giliarder1994@gmail.com>
2025-08-30 19:16:36 -0300

    aula 4 operações aritmética

---
+++ arquivos/aula04.js
@@ -0,0 +1,9 @@
+let num1=0, num2=0, res=0
+
+num1=10
+num1++ // num1+=1 ou num1 = num1 + 1
+//num1+=5 >> num1=num1+5
+
+console.log(num1)
+
+// o mesmo vale para as demais operadores no incremento ou decremento
