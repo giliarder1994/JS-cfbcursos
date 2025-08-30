d0c5a88ec213c1a9159c20b820f4e2ef2e72a118
Giliarde da Silva Rodrigues <giliarder1994@gmail.com>
2025-08-30 19:37:01 -0300

    atualizando a aula 05

--- arquivos/aula05.js
+++ arquivos/aula05.js
@@ -7,3 +7,8 @@ OPERADORES RELACIONAIS
 == igual
 != diferente
 */
+
+/*
+= é sinal de atribuição, quando uma variavel recebe um valor
+== é sinal de comparação, estou comparando o valor de uma variavel com o de outra variavel
+*/
