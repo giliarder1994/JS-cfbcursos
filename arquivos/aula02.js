8097a1c701c3cb345b7d077ecf766f8fe296e6b2
Giliarde da Silva Rodrigues <giliarder1994@gmail.com>
2025-08-30 17:46:23 -0300

    aula 02 - modo estrito ou restrito

---
+++ arquivos/aula02.js
@@ -0,0 +1,8 @@
+'use strict' // Modo estrito ou  restrito 
+
+let nome = 'Giliarde'
+console.log('CFBCursos')
+console.log(nome)
+console.log("Nome:" + ' ' + nome)
+
+//strict - não permite que uma variavel nao declarada funcione, dá erro
