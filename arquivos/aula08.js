9a5a132da6bc9f884699f049684339d02c6129b3
Giliarde da Silva Rodrigues <giliarder1994@gmail.com>
2025-09-01 21:46:04 -0300

    operadores bitwise

---
+++ arquivos/aula08.js
@@ -0,0 +1,16 @@
+/*
+OPERADORES BITWISE: ele faz a operação no bit a bit do elemento
+& - and e
+| - or ou
+^ - sor exclusivo
+<< - deslocar para esquerda (dobra o valor declarado)
+>> - deslocar para direita (corta pela metade o valor declarado)
+*/
+
+
+let n1=10
+let n2=11
+
+let res = n1 & n2
+
+console.log(res)
