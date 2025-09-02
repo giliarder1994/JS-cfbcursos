883e95951d463bdddb6b9de1590214a7596c5d26
Giliarde da Silva Rodrigues <giliarder1994@gmail.com>
2025-09-01 22:16:52 -0300

    aula 10 operadores ternarios

---
+++ arquivos/aula10.js
@@ -0,0 +1,23 @@
+//OPERADORES TERNARIOS:
+/*
+let num=10
+let res=num%2
+
+if (res==0) {
+    console.log("Par")
+}else {
+    console.log("Impar")
+}
+*/
+
+let num=13
+res=(!(num%2) ? "Par" : "Impar")
+console.log(res)
+
+//0 - False
+//1 - True
+
+/*Sintaxe da operação ternaria
+
+Teste Lógico ? se verdadeiro : se falso
+*/
